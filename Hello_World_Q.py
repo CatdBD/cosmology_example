eeaccd16243de8990992efc661e84252483d1797
Catherine de Burgh-Day <clatence@gmail.com>
2015-10-12 15:46:51 +1100

    Make some simple hello world scripts for the Intro Session

---
+++ Hello_World_Q.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+'''A simple Hello World function'''
+print 'What is your name? Please type it...'
+name = raw_input('Name: ')
+print 'Hello, '+str(name)+'!'
+
---
+++ Hello_World_Q_fn.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def main():
+	'''A simple Hello World function'''
+	print 'What is your name? Please type it...'
+	name = raw_input('Name: ')
+	print 'Hello '+str(name)+'!'
+
+if __name__ == '__main__':
+    main()
